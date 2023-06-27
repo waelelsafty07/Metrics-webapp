1c94765977a4e509deeef53626e1ef14ad229bfc
Wael Elsafty <waelelsafty07@gmail.com>
2023-06-27 12:20:10 +0300

    finish: app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 
 import './App.css';
+import Navbar from './components/Navbar';
 import Detail from './pages/Detail/Detail';
 import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/details/:id" element={<Detail />} />
       </Routes>
     </div>
   );
