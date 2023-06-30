15921ec1ee2c039b86f442f8b41acb388f2b629d
Wael Elsafty <waelelsafty07@gmail.com>
2023-06-30 20:34:42 +0300

    Fix: remove unless comment

--- src/components/HomepageItem.js
+++ src/components/HomepageItem.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../styles/HomepageItem.css';
-// import company from '../../images/company.png';
 import { BsBuildings } from 'react-icons/bs';
 
 const HomepageItem = (props) => {
@@ -10,7 +9,6 @@ const HomepageItem = (props) => {
     <div className="container" data-testid="homepageitem">
       <div className="item">
         <span className="arrow">➜</span>
-        {/* <img className="icon" src={company} alt="company" /> */}
         <BsBuildings />
         <h1 className="title">{title}</h1>
       </div>
